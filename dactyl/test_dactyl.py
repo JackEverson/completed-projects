162d10b74da8a0191f53f0ac4ad852f2c9f3e8d9
Jack Everson <jack.everson@outlook.com>
2023-10-21 21:17:06 +1000

    prototyping dactyl again

--- dactyl/test_dactyl.py
+++ dactyl/test_dactyl.py
@@ -9,7 +9,7 @@ from kmk.scanners import DiodeOrientation
 keyboard = KMKKeyboard()
 #keyboard.debug_enabled = True
 
-#Redox has 7 columns on left and 7 columns on right (total 14, left to right) and 5 rows, as it is COL2ROW diodes the 1N4148 diodes attach to the rows with the black side of the diode attached to the row (not the switch)
+# COL2ROW diodes the 1N4148 diodes attach to the rows with the black side of the diode attached to the row (not the switch)
 keyboard.col_pins = (board.GP16,board.GP17,board.GP18,board.GP19,board.GP20,board.GP21)
 #Columns left to right      1         2         3         4         5         6         
 
@@ -21,10 +21,6 @@ keyboard.diode_orientation = DiodeOrientation.COL2ROW
 # Layers
 from kmk.modules.layers import Layers
 keyboard.modules.append(Layers())
-
-# Mouse keys
-from kmk.modules.mouse_keys import MouseKeys
-keyboard.modules.append(MouseKeys())
 
 # HoldTap
 from kmk.modules.holdtap import HoldTap
@@ -44,27 +40,27 @@ keyboard.keymap = [
                                                                             #
     KC.TAB,    KC.Q,       KC.W,       KC.E,       KC.R,       KC.T,        #             KC.EQL,                         KC.PGDN,    KC.Y,       KC.U,       KC.I,       KC.O,       KC.P,      KC.BSLS,
                                                                             #
-    KC.CAPS,   KC.A,       KC.S,       KC.D,       KC.F,       KC.G,        #            KC.LBRC,                            KC.END,     KC.H,      KC.J,        KC.K,       KC.L,       KC.SCLN,    KC.QUOTE,
+    KC.LCTRL,   KC.A,       KC.S,       KC.D,       KC.F,       KC.G,        #            KC.LBRC,                            KC.END,     KC.H,      KC.J,        KC.K,       KC.L,       KC.SCLN,    KC.QUOTE,
                                                                             #
     KC.LSHIFT, KC.Z,       KC.X,        KC.C,       KC.V,       KC.B,       #            KC.RBRC,                         KC.HOME,    KC.N,       KC.M,       KC.COMMA,   KC.DOT,     KC.SLSH,    KC.RSHIFT,
                                                                             #
-    XXXXX,      KC.LGUI,      XXXXX,      XXXXX,      KC.LBRC,    KC.RBRC,    #   KC.LCTRL,  tog_lay2,  KC.LGUI,     KC.LALT,  KC.LSHIFT,   KC.HT(KC.ENTER, tog_lay2),   KC.SPACE,   KC.BSPC, KC.HT(KC.DEL, tog_lay1), KC.LSHIFT,   KC.RALT,     KC.RGUI,    tog_lay2,    KC.RCTRL,
+    tog_lay1,      KC.LGUI,      KC.LALT,      KC.GRAVE,      XXXXX, XXXXX, # KC.LBRC,    KC.RBRC,    #   KC.LCTRL,  tog_lay2,  KC.LGUI,     KC.LALT,  KC.LSHIFT,   KC.HT(KC.ENTER, tog_lay2),   KC.SPACE,   KC.BSPC, KC.HT(KC.DEL, tog_lay1), KC.LSHIFT,   KC.RALT,     KC.RGUI,    tog_lay2,    KC.RCTRL,
     
-    KC.HT(KC.ENTER, tog_lay2),KC.SPACE, KC.LCTRL,KC.LALT, XXXXX,XXXXX,
+    KC.HT(KC.ENTER, tog_lay2),KC.SPACE, KC.TAB, KC.EQL, XXXXX,XXXXX,
     ],
 
     [#Layer 1: Motions
-    KC.RLD,  KC.F1,      KC.F2,      KC.F3,      KC.F4,      KC.F5,       #KC.F6,                          KC.F7,    KC.F8,      KC.F9,      KC.F10,     KC.F11,     KC.F12,      KC.MINUS,
+    KC.GRAVE,  KC.F1,      KC.F2,      KC.F3,      KC.F4,      KC.F5,       #KC.F6,                          KC.F7,    KC.F8,      KC.F9,      KC.F10,     KC.F11,     KC.F12,      KC.MINUS,
 
     XXXXX,    XXXXX,      XXXXX,      XXXXX,      XXXXX,      XXXXX,     
 
-    XXXXX,  XXXXX,XXXXX,XXXXX,XXXXX,XXXXX,  
+    KC.LCTRL,  XXXXX,XXXXX,XXXXX,XXXXX,XXXXX,  
 
-    XXXXX,    XXXXX,      XXXXX,      XXXXX,      XXXXX,       XXXXX,   
+    KC.LSHIFT,    XXXXX,      XXXXX,      XXXXX,      XXXXX,       XXXXX,   
     
-    XXXXX,    XXXXX,      XXXXX,      XXXXX,      XXXXX,       XXXXX,   
+    XXXXX,      KC.LGUI,      KC.LALT,      KC.GRAVE,      XXXXX, XXXXX,
 
-    XXXXX,    XXXXX,      XXXXX,      XXXXX,      XXXXX,       XXXXX,   
+    XXXXX, KC.SPACE, KC.TAB, KC.EQL, XXXXX,XXXXX,
     ],
 
     [#Layer 2: numbers 
